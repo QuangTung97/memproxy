422c1f591548adfde9f4e567b72d15942e0ac743
tung.tq <tung.tq@teko.vn>
2023-01-10 19:55:13 +0700

    Add MHash

--- mhash/mhash.go
+++ mhash/mhash.go
@@ -1,6 +1,7 @@
 package mhash
 
 import (
+	"context"
 	"github.com/QuangTung97/memproxy"
 	"github.com/QuangTung97/memproxy/item"
 )
@@ -11,19 +12,64 @@ type Null[T any] struct {
 	Data  T
 }
 
+// Bucket ...
+type Bucket[T any] struct {
+	Items  []T
+	Bitset [256]byte
+}
+
+// Marshal ...
+func (Bucket[T]) Marshal() ([]byte, error) {
+	// TODO
+	return nil, nil
+}
+
+// BucketKey ...
+type BucketKey[R item.Key] struct {
+	RootKey R
+	Hash    uint64
+}
+
+// String ...
+func (k BucketKey[R]) String() string {
+	return k.RootKey.String()
+}
+
+// Filler ...
+type Filler[T any, R any] func(ctx context.Context, rootKey R, hash uint64) func() (Bucket[T], error)
+
 // Hash ...
-type Hash[T item.Value, K item.Key] struct {
+type Hash[T item.Value, R item.Key, K item.Key] struct {
 	sess     memproxy.Session
 	pipeline memproxy.Pipeline
+	getKey   func(v T) K
+	filler   Filler[T, R]
+
+	bucketItem *item.Item[Bucket[T], BucketKey[R]]
 }
 
 // New ...
-func New[T item.Value, K item.Key](
+func New[T item.Value, R item.Key, K item.Key](
 	sess memproxy.Session,
 	pipeline memproxy.Pipeline,
-) *Hash[T, K] {
-	return &Hash[T, K]{
+	getKey func(v T) K,
+	unmarshaler item.Unmarshaler[Bucket[T]],
+	filler Filler[T, R],
+) *Hash[T, R, K] {
+	var itemFiller item.Filler[Bucket[T], BucketKey[R]] = func(
+		ctx context.Context, key BucketKey[R],
+	) func() (Bucket[T], error) {
+		return filler(ctx, key.RootKey, key.Hash)
+	}
+
+	return &Hash[T, R, K]{
 		sess:     sess,
 		pipeline: pipeline,
+		getKey:   getKey,
+		filler:   filler,
+
+		bucketItem: item.New[Bucket[T], BucketKey[R]](
+			sess, pipeline, unmarshaler, itemFiller,
+		),
 	}
 }
